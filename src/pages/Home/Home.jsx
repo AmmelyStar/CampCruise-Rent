566f0f75e6c2fccd539d6ee4bfcf6ae1ec74b952
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2024-04-22 21:44:57 +0200

    edit home bg

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,10 @@ import {
   HomeStyle,
   Text,
   Title,
-  // Box,
-  // Wrap,
-  Wrapper,
-  Container,
-
-  ImageOverlay,
+   ImageOverlay,
 } from './HomeStyle';
 import Footer from 'components/Footer/Footer';
-import bg from '../../img/bgHome2.png';
+import bg from '../../img/bgHome23.png';
 
 const Home = () => {
   return (
@@ -24,41 +19,9 @@ const Home = () => {
           
               We are delighted to welcome you to our website, where you can find
               everything you need for a comfortable and exciting outdoor
-              adventure.
-             
-              {/* We specialize in camper van and car rentals, offering a wide range
-              of vehicles for any kind of adventure. */}
-         
+              adventure.         
           </Text>
         </ImageOverlay>
-        <Container>
-          <Wrapper>
-            {/* <Box>
-              <Wrap>
-                <strong>Vehicle Variety:</strong>
-                <br />
-                <br /> Compact vans to spacious RVs for every adventure.
-              </Wrap>
-              <Wrap>
-                <strong>Quality and Safety:</strong>
-                <br />
-                <br /> Reliable vehicles with regular maintenance for your
-                comfort and safety.
-              </Wrap>
-              <Wrap>
-                <strong>Convenience:</strong>
-                <br />
-                <br /> Easy process, convenient pickup and drop-off locations.
-              </Wrap>
-              <Wrap>
-                <strong>Expert Support:</strong>
-                <br />
-                <br /> Advice on routes and camping spots, technical assistance
-                available.
-              </Wrap>
-            </Box> */}
-          </Wrapper>
-        </Container>
       </HomeStyle>
       <Footer />
     </>
